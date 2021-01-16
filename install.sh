833d181913bf6de2116251479412284d0ef8a9b4
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2021-01-16 19:11:01 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install upgrad -y
+pkg install cowsay -y
+pkg install mpv -y
+apt install figlet -y
+
+gem install lolcat -y
+
+figlet -f big Done !!! | lolcat -a -d 3
+mv MR-HK.cow /data/data/com.termux/files/usr/share/cows
