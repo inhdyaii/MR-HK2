2a2a3b8849d74b2c58a454f7204a24dda0f67104
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2021-01-16 19:36:12 +0200

    Update MR-HK.sh

--- MR-HK.sh
+++ MR-HK.sh
@@ -23,10 +23,10 @@ printf '\e[32m[\e[39m*\e[32m]\e[36m     whats your mp3 :- \e[0m' && read mp3
 
 printf '\e[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m\n'
 
-cd /sdcard/Download && cp $mp3 /data/data/com.termux/files/home/MR-HK2/mp3 && cd /data/data/com.termux/files/home && rm MR-HKjo.sh && cd /data/data/com.termux/files/hom/MR-HK2 || cd /data/data/com.termux/files/home/MR-HK && rm MR-HKjo.sh && touch MR-HKjo.sh || cd /data/data/com.termux/files/home/MR-HK && touch MR-HKjo.sh
+cd /sdcard/Download && cp $mp3 /data/data/com.termux/files/home/MR-HK2/SS && cd /data/data/com.termux/files/home && rm MR-HKjo.sh && cd /data/data/com.termux/files/hom/MR-HK2 || cd /data/data/com.termux/files/home/MR-HK && rm MR-HKjo.sh && touch MR-HKjo.sh || cd /data/data/com.termux/files/home/MR-HK2 && touch MR-HKjo.sh
 
 echo -e """
-mpv /data/data/com.termux/files/home/MR-HK2/mp3/$mp3 >/data/data/com.termux/files/home/MR-HK2/mp3/ww 2>&1
+mpv /data/data/com.termux/files/home/MR-HK2/SS/$mp3 >/data/data/com.termux/files/home/MR-HK2/SS/ww 2>&1
 cowsay -f MR-HK.cow $name | lolcat -p
 
 figlet $name
